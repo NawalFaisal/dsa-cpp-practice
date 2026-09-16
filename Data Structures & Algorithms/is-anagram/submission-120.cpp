83ce6bdccb6340da5cea09880ba021798b8bb7ac
NawalFaisal <nawal.mohamuud@uleth.ca>
2026-09-16 16:48:35 -0600

    Add: is-anagram - submission-120

---
+++ Data Structures & Algorithms/is-anagram/submission-120.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        //reguar array
+        vector<int>count(26,0);
+
+        for(char c: s){
+            count[c - 'a']++;
+        }
+        for(char c : t){
+            count[c - 'a']--;
+        }
+        for(int n : count){
+            if(n!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
