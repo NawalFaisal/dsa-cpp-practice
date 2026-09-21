db7c868819d8e3d7fdebec94e016b5caa9a7b5f6
NawalFaisal <nawal.mohamuud@uleth.ca>
2026-09-21 17:22:54 -0600

    Add: anagram-groups - submission-33

---
+++ Data Structures & Algorithms/anagram-groups/submission-33.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> group;
+        for(string s : strs){
+            string key = s;
+            sort(begin(key), end(key));
+            group[key].push_back(s);
+        }
+        vector<vector<string>> result;
+
+        for(auto& pair : group){
+            result.push_back(pair.second);
+        }
+        return result;
+
+        
+    }
+};
