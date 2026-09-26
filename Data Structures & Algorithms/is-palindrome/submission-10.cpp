8d8825bde8443ea98feaaf78c864b397f9f77ca9
NawalFaisal <nawal.mohamuud@uleth.ca>
2026-09-26 07:12:07 -0600

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+class Solution {
+public:
+    bool isPalindrome(string s) {        
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right){
+            while(left < right and not isalnum(s[left])){
+                left++;
+            }
+            while(left < right and not isalnum(s[right])){
+                right--;
+            }
+            if (tolower(s[left]) == tolower(s[right])){
+                left++;
+                right--;
+                }else{
+                    return false;
+                }
+        } 
+        return true;
+    }  
+};
